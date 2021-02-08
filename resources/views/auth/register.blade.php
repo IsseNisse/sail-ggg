23a53fac6b757826a948682c3674772b723f8f14
IsseNisse <isak.vindeln@gmail.com>
2021-02-08 14:54:03 +0100

    har börjat lägga till lite lätt styling och och fixa länkar mellan sidor

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,9 +19,6 @@
                 <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
             </div>
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
 
                 <x-button class="ml-4">
                     {{ __('Register') }}
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/register');
+});
+
+Route::get('/game', function () {
+    return view('game');
 });
 
 Route::get('/dashboard', function () {
